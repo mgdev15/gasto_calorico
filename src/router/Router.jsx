feb4845c7b92f44aab00c39297deee7fde1b3288
mgdev15 <miguelpilonetto8@gmail.com>
2025-09-13 16:06:06 -0300

    Router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,44 +2,37 @@ import { lazy } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import PageWrapper from "../wrapper/PageWrapper";
-import { createContext, useState } from "react";
 
 const Home = lazy(() => import('../pages/Home'));
 const Gasto = lazy(() => import('../pages/Gasto'));
-export const GenderContext = createContext();
 
 export default function Router(){
 
     const location = useLocation();
-    const [gender, setGender] = useState(null);
 
     return(
 
         <AnimatePresence mode="wait">
 
-            <GenderContext.Provider value={{ gender, setGender }}>
+            <Routes location={location} key={location.pathname}>
 
-                <Routes location={location} key={location.pathname}>
+                <Route path="/" 
+                    element={
+                        <PageWrapper>
+                            <Home />
+                        </PageWrapper>
+                    } 
+                />
 
-                    <Route path="/" 
-                        element={
-                            <PageWrapper>
-                                <Home />
-                            </PageWrapper>
-                        } 
-                    />
+                <Route path="/gasto-calorico" 
+                    element={
+                        <PageWrapper>
+                            <Gasto />
+                        </PageWrapper>
+                    } 
+                />
 
-                    <Route path="/gasto-calorico" 
-                        element={
-                            <PageWrapper>
-                                <Gasto />
-                            </PageWrapper>
-                        } 
-                    />
-
-                </Routes>
-
-            </GenderContext.Provider>
+            </Routes>
 
         </AnimatePresence>
 
